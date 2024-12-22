8cb9d452f2e4c8b51b809e3e8c08493063efa0ab
kalilinux429 <suchitkapale@gmail.com>
2024-12-22 20:31:30 +0530

    initial commit

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,21 +1,27 @@
 import { createContext, useContext, useReducer } from "react";
-import faker from "faker";
+import { faker } from '@faker-js/faker';  // Correct import
 import { cartReducer, productReducer } from "./Reducers";
 
+// Create the context for Cart
 const Cart = createContext();
+
+// Set a seed for faker to generate consistent data
 faker.seed(99);
 
+
 const Context = ({ children }) => {
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
+  // Generate fake product data
+  const products = [...Array(48)].map(() => ({
+    id: faker.string.uuid(),  // Correct usage
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    image: faker.image.url(),
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
-    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
   }));
 
+  // Set up state management with useReducer for cart and products
   const [state, dispatch] = useReducer(cartReducer, {
     products: products,
     cart: [],
@@ -37,6 +43,7 @@ const Context = ({ children }) => {
   );
 };
 
+// Custom hook to access cart state
 export const CartState = () => {
   return useContext(Cart);
 };
